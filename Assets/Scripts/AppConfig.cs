4bb092a51f75e3ca81d1326ecd39eff210f0fd0b
Keijiro Takahashi <keijiro@gmail.com>
2024-09-17 22:33:43 +0900

    Editor/Player only timelines

--- Assets/Scripts/AppConfig.cs
+++ Assets/Scripts/AppConfig.cs
@@ -2,7 +2,22 @@ using UnityEngine;
 
 public sealed class AppConfig : MonoBehaviour
 {
-#if !UNITY_EDITOR
+    [SerializeField] GameObject _editorOnlyObject = null;
+    [SerializeField] GameObject _playerOnlyObject = null;
+
+#if UNITY_EDITOR
+    void OnEnable()
+    {
+        _editorOnlyObject.SetActive(true);
+        _playerOnlyObject.SetActive(false);
+    }
+#else
+    void OnEnable()
+    {
+        _editorOnlyObject.SetActive(false);
+        _playerOnlyObject.SetActive(true);
+    }
+
     void Start()
       => Cursor.visible = false;
 #endif
